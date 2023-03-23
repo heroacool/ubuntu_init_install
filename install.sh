d52ee40ff5ca3e84655495f6cb5c7ece9bb2572a
heroacool <heroacool@gmail.com>
2023-03-23 19:09:06 +0800

    init

---
+++ install.sh
@@ -0,0 +1,46 @@
+# ubuntu alibaba source, please update the /etc/apt/source.list
+
+# deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+# deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+# deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+
+sudo apt-get install -y git \
+    vlc \
+    terminator \
+    kazam \
+    build-essential virtualbox \
+    cmake-curses-gui \
+    net-tools \
+    curl \
+    vim \
+    meshlab \
+    libgoogle-glog-dev \
+    libgflags-dev \
+    libsuitesparse-dev \
+    libatlas-base-dev \
+    openssh-server \
+    pcl-tools \
+    htop \
+
+# CMake
+sudo apt-get install cmake
+# google-glog + gflags
+sudo apt-get install libgoogle-glog-dev libgflags-dev
+# BLAS & LAPACK
+sudo apt-get install libatlas-base-dev
+# Eigen3
+sudo apt-get install libeigen3-dev
+# SuiteSparse and CXSparse (optional)
+sudo apt-get install libsuitesparse-dev
+
+
+
+
+
